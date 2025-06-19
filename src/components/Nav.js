ce540c4fef3e7156d9c0ba4acd150110d12e5864
Hyrox_ <thehyrox@gmail.com>
2025-06-19 18:12:42 +0200

    Add back button on menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -27,7 +27,18 @@ export class Nav {
                     <a href="#" id="nav-reset" title="Reset">
                         <svg viewBox="0 0 24 24"><path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"/></svg>
                     </a>
+                    <a href="javascript:history.back()" title="Go Back">
+                        <svg viewBox="0 0 24 24"><path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/></svg>
+                    </a>
                 </div>`;
+        } else {
+            leftControlsHTML = `
+                <div class="nav-links track-controls-left">
+                    <a href="javascript:history.back()" title="Go Back">
+                        <svg viewBox="0 0 24 24"><path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/></svg>
+                    </a>
+                </div>
+            `;
         }
 
         const triggerHTML = `
